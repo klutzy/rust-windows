8ff06d002d24e5ab7efc76d5d5f1bd0049ab030a
klutzy <klutzytheklutzy@gmail.com>
2013-06-30 20:09:47 +0900

    Introduce trait Window/WindowUtil

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,6 +1,19 @@
 extern mod win32;
 
+use win32::window::*;
+use win32::ll::*;
+
+struct MainWindow {
+    raw: HWND,
+}
+
+impl win32::window::Window for MainWindow {
+    fn hwnd(&self) -> HWND {
+        self.raw
+    }
+}
+
 fn main() {
-    let non = win32::window::Window::null();
+    let non = win32::window::null();
     non.message_box("Hello World", "hello world");
 }
--- window.rs
+++ window.rs
@@ -1,26 +1,40 @@
 use core::ptr;
 use ll::*;
 
-pub struct Window {
+pub trait Window {
+    fn hwnd(&self) -> HWND;
+}
+
+pub struct EmptyWindow {
     raw: HWND,
 }
 
-impl Window {
-    pub fn null() -> Window {
-        unsafe {
-            Window {
-                raw: ptr::null(),
-            }
+impl Window for EmptyWindow {
+    fn hwnd(&self) -> HWND {
+        self.raw
+    }
+}
+
+pub fn null() -> EmptyWindow {
+    unsafe {
+        EmptyWindow {
+            raw: ptr::null(),
         }
     }
+}
 
+pub trait WindowUtil {
+    pub fn message_box(&self, msg: &str, title: &str);
+}
+
+impl<T: Window> WindowUtil for T {
     pub fn message_box(&self, msg: &str, title: &str) {
         let mut wmsg = str::to_utf16(msg);
         wmsg.push(0u16);
         let mut wtitle = str::to_utf16(title);
         wtitle.push(0u16);
         unsafe {
-            user32::MessageBoxW(self.raw,
+            user32::MessageBoxW(self.hwnd(),
             vec::raw::to_ptr(wmsg), vec::raw::to_ptr(wtitle),
             0u32);
         }
