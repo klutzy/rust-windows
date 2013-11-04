e49bb690e75205bccd5fd2f99784c1f5408754d1
klutzy <klutzytheklutzy@gmail.com>
2013-11-04 20:39:30 +0900

    Add wnd_proc! macro

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,4 +1,4 @@
-#[feature(globs)];
+#[feature(globs, macro_rules)];
 
 extern mod win32;
 
@@ -11,6 +11,9 @@ use win32::resource::*;
 use win32::window::*;
 use win32::gdi::WindowPaint;
 use win32::font::Font;
+
+#[path = "../wnd_proc_macro.rs"]
+mod macro;
 
 // TODO duplicate of hello.rc
 static IDI_ICON: int = 0x101;
@@ -25,6 +28,8 @@ struct MainFrame {
     edit: Cell<Window>,
     font: Cell<Font>,
 }
+
+wnd_proc!(MainFrame, win, WM_CREATE, WM_DESTROY, WM_SIZE, WM_SETFOCUS, WM_PAINT)
 
 impl OnCreate for MainFrame {
     fn on_create(&self, _cs: &CREATESTRUCT) -> bool {
@@ -93,48 +98,6 @@ impl OnFocus for MainFrame {
         do self.edit.with_ref |edit| {
             edit.set_focus();
         }
-    }
-}
-
-impl WindowImpl for MainFrame {
-    fn wnd<'a>(&'a self) -> &'a Window {
-        &self.win
-    }
-
-    fn wnd_mut<'a>(&'a mut self) -> &'a mut Window {
-        &mut self.win
-    }
-
-    fn wnd_proc(&self, msg: UINT, w: WPARAM, l: LPARAM) -> LRESULT {
-        if msg == 0x0001 { // WM_CREATE
-            let cs = unsafe {
-                let pcs = std::cast::transmute::<LPARAM, *CREATESTRUCT>(l);
-                &(*pcs)
-            };
-            let ret = self.on_create(cs);
-            return if ret { 0 as LRESULT } else { -1 as LRESULT };
-        }
-        if msg == 0x0002 { // WM_DESTROY
-            self.on_destroy();
-            return 0 as LRESULT;
-        }
-        if msg == 0x0005 { // WM_SIZE
-            let l = l as u32;
-            let width = (l & 0xFFFF) as int;
-            let height = (l >> 16) as int;
-            self.on_size(width, height);
-            return 0 as LRESULT;
-        }
-        if msg == 0x0007 { // WM_SETFOCUS
-            let w = Window { wnd: w as HWND };
-            self.on_focus(w);
-            return 0 as LRESULT;
-        }
-        if msg == 0x000F { // WM_PAINT
-            self.on_paint();
-            return 0 as LRESULT;
-        }
-        win32::def_window_proc(self.wnd().wnd, msg, w, l)
     }
 }
 
