cfd43ca57c3336377fab3d1d183c5b359c576414
Gigih Aji Ibrahim <bungcip@gmail.com>
2014-07-23 11:02:58 +0700

    fix macro hygiene issues

--- macros.rs
+++ macros.rs
@@ -2,14 +2,14 @@
 
 #[macro_export]
 macro_rules! wnd_proc_thunk(
-    (WM_CREATE) => (
-        if msg == 0x0001 { // WM_CREATE
+    ($self_:ident, $msg:ident, $w:ident, $l:ident, WM_CREATE) => (
+        if $msg == 0x0001 { // WM_CREATE
             let cs = unsafe {
                 let pcs = ::std::mem::transmute::<::windows::ll::types::LPARAM,
-                                                   *::windows::ll::all::CREATESTRUCT>(l);
+                                                   *const ::windows::ll::all::CREATESTRUCT>($l);
                 &(*pcs)
             };
-            let ret = self.on_create(cs);
+            let ret = $self_.on_create(cs);
             if ret {
                 return 0 as ::windows::ll::types::LRESULT;
             } else {
@@ -17,31 +17,31 @@ macro_rules! wnd_proc_thunk(
             }
         }
     );
-    (WM_DESTROY) => (
-        if msg == 0x0002 { // WM_DESTROY
-            self.on_destroy();
+    ($self_:ident, $msg:ident, $w:ident, $l:ident, WM_DESTROY) => (
+        if $msg == 0x0002 { // WM_DESTROY
+            $self_.on_destroy();
             return 0 as ::windows::ll::types::LRESULT;
         }
     );
-    (WM_SIZE) => (
-        if msg == 0x0005 { // WM_SIZE
-            let l = l as u32;
+    ($self_:ident, $msg:ident, $w:ident, $l:ident, WM_SIZE) => (
+        if $msg == 0x0005 { // WM_SIZE
+            let l = $l as u32;
             let width = (l & 0xFFFF) as int;
             let height = (l >> 16) as int;
-            self.on_size(width, height);
+            $self_.on_size(width, height);
             return 0 as ::windows::ll::types::LRESULT;
         }
     );
-    (WM_SETFOCUS) => (
-        if msg == 0x0007 { // WM_SETFOCUS
-            let w = ::windows::window::Window { wnd: w as ::windows::ll::types::HWND };
-            self.on_focus(w);
+    ($self_:ident, $msg:ident, $w:ident, $l:ident, WM_SETFOCUS) => (
+        if $msg == 0x0007 { // WM_SETFOCUS
+            let w = ::windows::window::Window { wnd: $w as ::windows::ll::types::HWND };
+            $self_.on_focus(w);
             return 0 as ::windows::ll::types::LRESULT;
         }
     );
-    (WM_PAINT) => (
-        if msg == 0x000F { // WM_PAINT
-            self.on_paint();
+    ($self_:ident, $msg:ident, $w:ident, $l:ident, WM_PAINT) => (
+        if $msg == 0x000F { // WM_PAINT
+            $self_.on_paint();
             return 0 as ::windows::ll::types::LRESULT;
         }
     );
@@ -63,7 +63,7 @@ macro_rules! wnd_proc(
         fn wnd_proc(&self, msg: ::windows::ll::types::UINT, w: ::windows::ll::types::WPARAM,
                     l: ::windows::ll::types::LPARAM) -> ::windows::ll::types::LRESULT {
             $(
-                wnd_proc_thunk!($msg)
+                wnd_proc_thunk!(self, msg, w, l, $msg)
             )+
             ::windows::def_window_proc(self.wnd().wnd, msg, w, l)
         }
