eabc401947bfdedc813cb88a7efd3157d8d257d8
Peter Reid <peter.d.reid@gmail.com>
2015-01-01 09:08:02 -0500

    Add ASL2/MIT license
    This mirrors the license that rust uses.
    
    The relevant contributors approved this in
    https://github.com/klutzy/rust-windows/issues/6

--- ll/gdi.rs
+++ ll/gdi.rs
@@ -1,3 +1,12 @@
+// Copyright 2015 The Rust-Windows Project Developers. See the
+// COPYRIGHT file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use libc::c_int;
 use libc::types::os::arch::extra::{LPCWSTR, LPWSTR};
 use ll::types::{HDC, BOOL, HBITMAP, HGDIOBJ, DWORD, HFONT};
